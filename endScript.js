41a233e8d3eaa21a6b973172cb9f22d57738251a
Nolan Dey <ndey96@gmail.com>
2016-06-05 12:50:55 -0400

    add contentBlocker.json, add stylesheet

--- endScript.js
+++ endScript.js
@@ -1,10 +1,9 @@
 document.body.contentEditable = true;
-var element = document.getElementById("contentArea");
+var element = document.getElementById("stream_pagelet");
 if (element === null) {
 	console.log("Failure");
+} else {
+	console.log(element);
+	var imageUrl = "http://cdn.lamborghini.com/content/models/Centenario_LP_770-4/Lamborghini-Centenario-geneva-motorshow-2016-Frontslide-.jpg";
+	element.innerHTML = "<img src=" + imageUrl + " width=\"500\"</img>";
 }
-console.log(element);
-var imageUrl = "http://cdn.lamborghini.com/content/models/Centenario_LP_770-4/Lamborghini-Centenario-geneva-motorshow-2016-Frontslide-.jpg";
-// console.log("style: ", element.style);
-var imgStyleString = ""
-element.innerHTML = "<img src=" + imageUrl + " width=\"500\"</img>";
