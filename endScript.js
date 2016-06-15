de732075c957d3e5bc945159bf7888fff77cce0e
Nolan Dey <ndey96@gmail.com>
2016-06-15 19:46:22 -0400

    Remove contentBlocker, endscript. Track info.plist

--- endScript.js
+++
@@ -1,9 +0,0 @@
-document.body.contentEditable = true;
-var element = document.getElementById("stream_pagelet");
-if (element === null) {
-	console.log("Failure");
-} else {
-	console.log(element);
-	var imageUrl = "http://cdn.lamborghini.com/content/models/Centenario_LP_770-4/Lamborghini-Centenario-geneva-motorshow-2016-Frontslide-.jpg";
-	element.innerHTML = "<img src=" + imageUrl + " width=\"500\"</img>";
-}
